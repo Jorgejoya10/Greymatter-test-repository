cd4a8146a793ac313000952e413475cc3dd2aebb
Jorge Joya <jorgejoya.100@gmail.com>
2025-12-10 16:27:23 -0600

    Estilos css agregados

--- aem-demo/assets/js/main.js
+++ aem-demo/assets/js/main.js
@@ -1,4 +1,42 @@
+async function loadFragment(slotId, url, append = false) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP ${res.status} al cargar ${url}`);
+    }
 
+    const html = await res.text();
+    const slot = document.getElementById(slotId);
 
+    if (!slot) return;
 
-console.log('Hello from AEM Demo main.js!');
+    if (append) {
+      slot.innerHTML += html;
+    } else {
+      slot.innerHTML = html;
+    }
+  } catch (err) {
+    console.error(`[AEM-DEMO] Error cargando fragmento: ${url}`, err);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // 1. Header global (layout)
+  await loadFragment("header-slot", "./aem-demo/templates/header.html");
+
+  // 2. Contenido de la HOME (en orden)
+  await loadFragment("content-slot", "./aem-demo/components/home/hero.html");
+  await loadFragment(
+    "content-slot",
+    "./aem-demo/components/home/carrusel.html",
+    true
+  );
+  await loadFragment(
+    "content-slot",
+    "./aem-demo/components/home/services.html",
+    true
+  );
+
+  // 3. Footer global (layout)
+  await loadFragment("footer-slot", "./aem-demo/templates/footer.html");
+});
